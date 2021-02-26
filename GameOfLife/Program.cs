4888d4646339bcd2413acf6211eacda627716764
Jyotirmaya Sahu <jyotirmayasahu38@gmail.com>
2021-02-26 22:23:57 +0530

    Fixed User input

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using GameUtilities;
 
 namespace GameOfLife
@@ -7,17 +8,50 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
+            List<Cell> inputCoordinates = new List<Cell>();
+            int x, y;
+
+            Console.WriteLine("Provide coordinates of cells (separated by comma): (Enter 0 to stop input)");
+            string input = "";
+            do
+            {
+                input = Console.ReadLine();
+                if (input == "0") continue;
+                if(!input.Contains(","))
+                {
+                    Console.Error.WriteLine("Kindly provide inputs separated by comma (,) !");
+                    continue;
+                }
+                string[] coords = input.Split(",");
+                try
+                {
+                    x = Convert.ToInt32(coords[0]);
+                    y = Convert.ToInt32(coords[1]);
+
+                    inputCoordinates.Add(new Cell(x, y));
+                }
+                catch (FormatException)
+                {
+                    Console.Error.WriteLine("Kindly provide only numeric values!");
+                }
+                catch(Exception)
+                {
+                    Console.Error.WriteLine("Invalid input! Try Again.");
+                }
+
+            }
+            while (input != "0");
+
             Game game = new Game();
-            game.SetInitialState(new Cell[] {
-                new Cell(1,1),
-                new Cell(1,0),
-                new Cell(1,2),
-            });
+            game.SetInitialState(inputCoordinates.ToArray());
 
+            Console.Clear();
+
+            Console.WriteLine("Initial State\n***************");
             Console.WriteLine(game.getCurrentState());
-
+            Console.WriteLine("\n");
             game.ProceedTicks(1);
-            Console.WriteLine("Result");
+            Console.WriteLine("Result\n***************");
             Console.WriteLine(game.getCurrentState());
         }
     }
